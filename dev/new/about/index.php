c9cf3ab1f6f1371f2c15e787317f901622db6231
alexjvan <Dreamonclaids@gmail.com>
2020-10-07 10:07:08 -0700

    Work a lil on about

--- dev/new/about/index.php
+++ dev/new/about/index.php
@@ -6,7 +6,73 @@
 	<?php include $prefix."global/head.php" ?>
 	<body>
 		<?php include $prefix."global/header.php" ?>
+		<div id="aboutpanel">
+			<div id="image">
+				<img id="meimg" src="https://via.placeholder.com/200x200/b4b4b4" />
+			</div>
+			<div id="name">
+				Alex Van Matre
+			</div>
+		</div>
+		<div id="content">
+			<div id="onpage">
+				<div id="aboutme" class="aboutportion">
+					<div id="elevatorpitch">
+						Hello there! My name is Alex, and I am a software and full stack developer from Bothell, Washington.
+					</div>
+					<div id="values">
 
+					</div>
+				</div>
+				<div id="instfeed" class="aboutportion">
+
+				</div>
+				<div id="passions" class="aboutportion">
+					<div class="passionitem">
+						<!-- COFFEE -->
+					</div>
+					<div class="passionitem">
+						<!-- CINEMA -->
+					</div>
+					<div class="passionitem">
+						<!-- MUSIC -->
+					</div>
+					<div class="passionitem">
+						<!-- COFFEE -->
+					</div>
+				</div>
+				<div id="meddlingimage" class="aboutportion">
+					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
+				</div>
+				<div id="skills" class="aboutportion">
+
+				</div>
+				<div id="meddlingimage" class="aboutportion">
+					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
+				</div>
+				<div id="education" class="aboutportion">
+
+				</div>
+				<div id="meddlingimage" class="aboutportion">
+					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
+				</div>
+				<div id="work" class="aboutportion">
+
+				</div>
+				<div id="meddlingimage" class="aboutportion">
+					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
+				</div>
+				<div id="volunteerwork" class="aboutportion">
+
+				</div>
+				<div id="meddlingimage" class="aboutportion">
+					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
+				</div>
+				<div id="resume" class="aboutportion">
+
+				</div>
+			</div>
+		</div>
 		<?php include $prefix.'global/footer.php' ?>
 	</body>
 </html>
