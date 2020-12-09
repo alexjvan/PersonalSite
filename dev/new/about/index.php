b0b83868911eb3a103f99b76875fa43e9a79694f
alexjvan <Dreamonclaids@gmail.com>
2020-12-09 00:55:31 -0800

    Push some new site stuff bb

--- dev/new/about/index.php
+++ dev/new/about/index.php
@@ -17,17 +17,20 @@
 		<div id="content">
 			<div id="onpage">
 				<div id="aboutme" class="aboutportion">
-					<div id="elevatorpitch">
-						Hello there! My name is Alex, and I am a software and full stack developer from Bothell, Washington.
+					<div class="aptitle">
+						A little about me
 					</div>
-					<div id="values">
-
+					<div id="elevatorpitch">
+						Hello there! My name is Alex, and I am a software and full stack developer from Bothell, Washington. I have dabbled in twelve different programming languages, as well as Spanish. I am working towards a bachelors in Computer Science and Software Engineering while I enjoy being your friendly cashier, and barista, at Target. I am in the process of building my second full-production website, as well as my own programming language. I continue to do this as I work to have raised over twenty thousand dollars over the last seventeen years for the JDRF fund.
 					</div>
 				</div>
 				<div id="instfeed" class="aboutportion">
 
 				</div>
 				<div id="passions" class="aboutportion">
+					<div class="aptitle">
+						Some of my passions
+					</div>
 					<div class="passionitem">
 						<!-- COFFEE -->
 					</div>
@@ -38,38 +41,48 @@
 						<!-- MUSIC -->
 					</div>
 					<div class="passionitem">
-						<!--  -->
+						<!-- Longboarding -->
 					</div>
 				</div>
 				<div id="meddlingimage" class="aboutportion">
 					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
 				</div>
 				<div id="skills" class="aboutportion">
-
+					<div class="aptitle">
+						A glance at my skills
+					</div>
 				</div>
 				<div id="meddlingimage" class="aboutportion">
 					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
 				</div>
 				<div id="education" class="aboutportion">
-
+					<div class="aptitle">
+						Where I learned things
+					</div>
 				</div>
 				<div id="meddlingimage" class="aboutportion">
 					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
 				</div>
 				<div id="work" class="aboutportion">
-
+					<div class="aptitle">
+						Some of my work
+					</div>
 				</div>
 				<div id="meddlingimage" class="aboutportion">
 					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
 				</div>
 				<div id="volunteerwork" class="aboutportion">
-
+					<div class="aptitle">
+						Where I volunteer
+					</div>
 				</div>
 				<div id="meddlingimage" class="aboutportion">
 					<img id="mi-img" src="https://via.placeholder.com/2000x200/b4b4b4" />
 				</div>
 				<div id="resume" class="aboutportion">
-
+					<div class="aptitle">
+						My resume
+					</div>
 				</div>
 			</div>
 		</div>
