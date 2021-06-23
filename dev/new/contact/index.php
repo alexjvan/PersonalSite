1de2871732a77bc4f90ecd4d7b3e1459d66873f1
avm595 <avm595@uw.edu>
2021-06-23 16:16:22 -0700

    Work on Contact page

--- dev/new/contact/index.php
+++ dev/new/contact/index.php
@@ -7,10 +7,178 @@
 	<?php include $prefix."global/head.php" ?>
 	<body>
 		<?php include $prefix."global/header.php" ?>
-		<div class="title">
+		<div class="title dark">
 			Where to Find Me
 		</div>
+		<div class="section dark">
+			<div class="mobile">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/gmail.svg" />
+					<div>
+						Email
+					</div>
+					<a href="mailto:contact@alexvanmatre.com" target="_blank">
+						contact@alexvanmatre.com
+					</a>
+				</div>
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/linkedin.svg" />
+					<div>
+						LinkedIn
+					</div>
+					<div>
+						Alex Van Matre
+					</div>
+					<a href="https://www.linkedin.com/in/alexvanmatre/" target="_blank">
+						https://www.linkedin.com/in/alexvanmatre/
+					</a>
+				</div>
+				<div class="text">
+					<div class="title">
+						Find Me Professionally
+					</div>
+					Feel free to reach out to me at my public email address, <a href="mailto:contact@alexvanmatre.com">contact@alexvanmatre.com</a>. You can also connect with me on the more professional platform, <a href="https://www.linkedin.com/in/alexvanmatre/">LinkedIn</a>.
+				</div>
+			</div>
+			<div class="desktop">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/gmail.svg" />
+					<div>
+						Email
+					</div>
+					<a href="mailto:contact@alexvanmatre.com" target="_blank">
+						contact@alexvanmatre.com
+					</a>
+				</div>
+				<div class="text">
+					<div class="title">
+						Find Me Professionally
+					</div>
+					Feel free to reach out to me at my public email address, <a href="mailto:contact@alexvanmatre.com">contact@alexvanmatre.com</a>. You can also connect with me on the more professional platform, <a href="https://www.linkedin.com/in/alexvanmatre/">LinkedIn</a>.
+				</div>
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/linkedin.svg" />
+					<div>
+						LinkedIn
+					</div>
+					<div>
+						Alex Van Matre
+					</div>
+					<a href="https://www.linkedin.com/in/alexvanmatre/" target="_blank">
+						https://www.linkedin.com/in/alexvanmatre/
+					</a>
+				</div>
+			</div>
+		</div>
 		<div class="section">
+			<div class="mobile">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/github.svg" />
+					<div>
+						Github
+					</div>
+					<div>
+						@alexjvan
+					</div>
+					<a href="https://github.com/alexjvan" target="_blank">
+						https://github.com/alexjvan
+					</a>
+				</div>
+				<div class="text">
+					<div class="title">
+						The Work I Do
+					</div>
+					Want to take a little peek at some of the code I write for school and for fun? Take a look at the code I make public on <a href="https://github.com/alexjvan">Github</a>.
+				</div>
+			</div>
+			<div class="desktop">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/github.svg" />
+					<div>
+						Github
+					</div>
+					<div>
+						@alexjvan
+					</div>
+					<a href="https://github.com/alexjvan" target="_blank">
+						https://github.com/alexjvan
+					</a>
+				</div>
+				<div class="text" style="width: 78%">
+					<div class="title">
+						The Work I Do
+					</div>
+					Want to take a little peek at some of the code I write for school and for fun? Take a look at the code I make public on <a href="https://github.com/alexjvan">Github</a>.
+				</div>
+			</div>
+		</div>
+		<div class="section dark">
+			<div class="mobile">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/instagram.svg" />
+					<div>
+						Instagram
+					</div>
+					<div>
+						@poke.i.haunt.us
+					</div>
+					<a href="https://www.instagram.com/poke.i.haunt.us/" target="_blank">
+						https://www.instagram.com/poke.i.haunt.us/
+					</a>
+				</div>
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/twitter.svg" />
+					<div>
+						Twitter
+					</div>
+					<div>
+						@alextomator
+					</div>
+					<a href="https://twitter.com/alextomator" target="_blank">
+						https://twitter.com/alextomator
+					</a>
+				</div>
+				<div class="text">
+					<div class="title">
+						See Me In a Casual Setting
+					</div>
+					I may not be the most active poster, but you can always see what I am up to on Instagram <a href="https://www.instagram.com/poke.i.haunt.us/">@poke.i.haunt.us</a>, or on Twitter <a href="https://twitter.com/alextomator">@alextomator</a>.
+				</div>
+			</div>
+			<div class="desktop">
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/instagram.svg" />
+					<div>
+						Instagram
+					</div>
+					<div>
+						@poke.i.haunt.us
+					</div>
+					<a href="https://www.instagram.com/poke.i.haunt.us/" target="_blank">
+						https://www.instagram.com/poke.i.haunt.us/
+					</a>
+				</div>
+				<div class="text">
+					<div class="title">
+						See Me In a Casual Setting
+					</div>
+					I may not be the most active poster, but you can always see what I am up to on Instagram <a href="https://www.instagram.com/poke.i.haunt.us/">@poke.i.haunt.us</a>, or on Twitter <a href="https://twitter.com/alextomator">@alextomator</a>.
+				</div>
+				<div class="socmed">
+					<img src="../static/images/other-sites/artsy/twitter.svg" />
+					<div>
+						Twitter
+					</div>
+					<div>
+						@alextomator
+					</div>
+					<a href="https://twitter.com/alextomator" target="_blank">
+						https://twitter.com/alextomator
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="section disabled">
 			<div class="socmed">
 				<img src="../static/images/other-sites/artsy/gmail.svg" />
 				<div>
